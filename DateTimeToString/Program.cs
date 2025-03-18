6f2903c06310bdf5b73f3c1bf3d72fd3cca1f13e
GitHubList <GitHubList@Outlook.com>
2025-03-18 10:35:58 +0800

    添加项目文件。

---
+++ DateTimeToString/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DateTimeToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine(DateTime.Now.ToString("yyyy-dd-MM HH:mm:ss"));
+            Console.ReadKey();
+        }
+    }
+}
